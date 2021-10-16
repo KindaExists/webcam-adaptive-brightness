420325a3c305c8d0f4d0eaeb2bff188b87b5ff16
KindaExists <ehrencastillo@gmail.com>
2021-10-16 11:06:10 +0800

    seperate code to different modules

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
 
-import screen_brightness_control as sbc
-import cv2 as cv
-
-cv.namedWindow('webcam')
-vc = cv.VideoCapture(0)
-
-current = sbc.get_brightness(display=0)
-print(current)
-
-while True:
-    ret, frame = vc.read()
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv.imshow('webcam', frame)
-
-    if cv.waitKey(5) == 27 or cv.waitKey(5) == 113:
-        # "ESC" or "q" key
-        break
-
-
-vc.release()
-cv.destroyWindow('webcam')
+if __name__ == '__main__':
+    # This only runs if opened in this specific file
+    pass
