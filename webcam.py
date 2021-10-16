3d792796924249acb3fb545b12c581a7b20f8a6d
KindaExists <ehrencastillo@gmail.com>
2021-10-16 11:28:08 +0800

    add basic average value function

--- webcam.py
+++ webcam.py
@@ -1,5 +1,6 @@
 
 import cv2 as cv
+import numpy as np
 import time
 
 cv.namedWindow('webcam')
@@ -7,13 +8,25 @@ vc = cv.VideoCapture(0)
 
 pause_time = 1
 
+# ? Consider making this an async function
 while True:
-    # Get Webcam frame
+    # Gets webcam frame
     _, webcam_frame = vc.read()
 
-    # Convert frame to grayscale
+    # Converts webcam frame to grayscale
     gray_frame = cv.cvtColor(webcam_frame, cv.COLOR_BGR2GRAY)
+
+    # Display grayscaled webcam output (for testing)
     cv.imshow('webcam', gray_frame)
+
+    # Gets average (HSV) value of frame
+    # ! Consider not using the average value
+    # ! Averaging treats all values the same
+    # ! So white (255) will be treated the same as lower values
+    # ? Look into using the "dominant" value
+    avg_val_per_row = np.average(gray_frame, axis=0)
+    avg_val = np.average(avg_val_per_row, axis=0)
+    print(avg_val)
 
     if cv.waitKey(5) == 27 or cv.waitKey(5) == 113:
         # "ESC" or "q" key
