9059e489fcaa821a8a147f6d110b47599db58b1d
KindaExists <ehrencastillo@gmail.com>
2021-10-22 18:18:11 +0800

    clean code

--- webcam.py
+++ webcam.py
@@ -2,10 +2,8 @@
 import cv2 as cv
 # import numpy as np
 
-cv.namedWindow('webcam')
-vc = cv.VideoCapture(0)
 
-while True:
+def get_brightness(vc):
     # Gets webcam frame
     _, frame = vc.read()
 
@@ -18,11 +16,28 @@ while True:
 
     # Obtain the average Y' (Luma) of the image
     brightness = mean[0]
+    return brightness
 
-    # Code for exiting the webcam window
-    # "ESC" or "q" key
-    if cv.waitKey(5) == 27 or cv.waitKey(5) == 113:
-        break
 
-vc.release()
-cv.destroyWindow('webcam')
+if __name__ == '__main__':
+    # This only runs if opened in this specific file
+
+    # Setup for webcam input and display
+    cv.namedWindow('webcam')
+    vc = cv.VideoCapture(0)
+
+    while True:
+        if cv.waitKey(5) == 27 or cv.waitKey(5) == 113:
+            # Code for exiting the webcam window
+            # "ESC" or "q" key
+            break
+
+        try:
+            print(get_brightness(vc))
+        except Exception:
+            print('ERROR: Webcam Disconnected')
+            break
+
+    # Closes webcam input and display
+    vc.release()
+    cv.destroyWindow('webcam')
