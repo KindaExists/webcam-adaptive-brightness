fcafd47f1cb1fca43acf036029ad4c06d7ebff1d
KindaExists <ehrencastillo@gmail.com>
2021-10-22 17:24:27 +0800

    remove useless comments and change to YUV

--- webcam.py
+++ webcam.py
@@ -1,43 +1,28 @@
 
 import cv2 as cv
-import numpy as np
-import time
+# import numpy as np
 
 cv.namedWindow('webcam')
 vc = cv.VideoCapture(0)
 
 pause_time = 1
 
-# ? Consider making this an async function
 while True:
     # Gets webcam frame
-    _, webcam_frame = vc.read()
-
-    # Converts webcam frame to grayscale
-    gray_frame = cv.cvtColor(webcam_frame, cv.COLOR_BGR2GRAY)
+    _, frame = vc.read()
 
     # Display grayscaled webcam output (for testing)
-    cv.imshow('webcam', gray_frame)
+    cv.imshow('webcam', frame)
 
-    # Gets average (HSV) value of frame
-    # ! Consider not using the average value
-    # ! Averaging treats all values the same
-    # ! So white (255) will be treated the same as lower values
-    # ? Look into using the "dominant" value
-    avg_val_per_row = np.average(gray_frame, axis=0)
-    avg_val = np.average(avg_val_per_row, axis=0)
-    print(avg_val)
+    yuv_frame = cv.cvtColor(frame, cv.COLOR_BGR2YUV)
+    mean = cv.mean(yuv_frame)
+    brightness = mean[0]
+
+    print(brightness)
 
     if cv.waitKey(5) == 27 or cv.waitKey(5) == 113:
         # "ESC" or "q" key
         break
-
-    # Pause for length in seconds, pause time will be defined by the user
-    # ! Issue: "time.sleep" causes a freeze
-    # * Potential Solution: Do pauses based on time since last timeout
-    # *                     to avoid freezing the entire algorithm
-    time.sleep(pause_time)
-
 
 vc.release()
 cv.destroyWindow('webcam')
