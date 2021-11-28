01783cddc013970c9c840df1e156adb0165d8288
KindaExists <ehrencastillo@gmail.com>
2021-11-28 20:05:47 +0800

    clean webcam capture code

--- webcam_adaptive_brightness/webcam.py
+++ webcam_adaptive_brightness/webcam.py
@@ -1,6 +1,5 @@
 
 import cv2 as cv
-import numpy as np
 
 
 class Webcam:
@@ -9,13 +8,35 @@ class Webcam:
         self.cam = cam_ref
 
     def get_brightness(self):
-        frame = self.__get_values()
-        brightness = np.mean(frame)
+        frame = self.get_values()
+        brightness = cv.mean(frame)[0]
 
         return brightness
 
-    def get_capture(self):
+    def get_values(self):
+        # Converts image from BGR to Grayscale (Y')
+        cap = self.get_capture(exposure=False)
+        gray = cv.cvtColor(cap, cv.COLOR_BGR2GRAY)
+
+        return gray
+
+    def get_capture(self, exposure=True):
+        if not exposure:
+            # Saves exposure settings
+            def_auto = self.vc.get(cv.CAP_PROP_AUTO_EXPOSURE)
+            def_exposure = self.vc.get(cv.CAP_PROP_EXPOSURE)
+
+            # Sets exposure to 100% and auto-exposure to 0%
+            self.vc.set(cv.CAP_PROP_AUTO_EXPOSURE, 0.0)
+            self.vc.set(cv.CAP_PROP_EXPOSURE, 1.0)
+
         _, frame = self.vc.read()
+
+        if not exposure:
+            # Reverts exposure settings to original values
+            self.vc.set(cv.CAP_PROP_AUTO_EXPOSURE, def_auto)
+            self.vc.set(cv.CAP_PROP_EXPOSURE, def_exposure)
+
         return frame
 
     def open(self):
@@ -24,21 +45,8 @@ class Webcam:
     def release(self):
         self.vc.release()
 
-    def __get_values(self):
-        # Saves exposure settings
-        def_auto = self.vc.get(cv.CAP_PROP_AUTO_EXPOSURE)
-        def_exposure = self.vc.get(cv.CAP_PROP_EXPOSURE)
+    def show_image(self, window_name, frame):
+        cv.imshow(window_name, frame)
 
-        # Sets exposure to 100% and auto-exposure to 0%
-        self.vc.set(cv.CAP_PROP_AUTO_EXPOSURE, 0.0)
-        self.vc.set(cv.CAP_PROP_EXPOSURE, 1.0)
-
-        # Converts image from BGR to Grayscale
-        cap = self.get_capture()
-        gray = cv.cvtColor(cap, cv.COLOR_BGR2GRAY)
-
-        # Returns exposure settings to original values
-        self.vc.set(cv.CAP_PROP_AUTO_EXPOSURE, def_auto)
-        self.vc.set(cv.CAP_PROP_EXPOSURE, def_exposure)
-
-        return gray
+    def close_windows(self):
+        cv.destroyAllWindows()
