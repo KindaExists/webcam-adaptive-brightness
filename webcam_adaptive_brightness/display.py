fe17fd1804dccc5796ca287616e2723d8981f415
KindaExists <ehrencastillo@gmail.com>
2021-11-29 13:35:19 +0800

    switch from screen_brightness_controller to wmi

--- webcam_adaptive_brightness/display.py
+++ webcam_adaptive_brightness/display.py
@@ -1,23 +1,19 @@
-import screen_brightness_control as sbc
 import numpy as np
-
-
-def ambient_to_display(brightness, min_ambient, max_ambient,
-                       min_display, max_display):
-    new_display_brightness = round(np.interp(brightness,
-                                             [min_ambient, max_ambient],
-                                             [min_display, max_display]))
-    return new_display_brightness
+import wmi
 
 
 def set_brightness(brightness):
-    sbc.set_brightness(brightness)
+    win = wmi.WMI(namespace='wmi')
+    methods = win.WmiMonitorBrightnessMethods()[0]
+    methods.WmiSetBrightness(brightness, 0)
 
 
-def update_brightness(ambient_brightness,
-                      min_ambient, max_ambient,
-                      min_display, max_display):
-    new_brightness = ambient_to_display(ambient_brightness,
-                                        min_ambient, max_ambient,
-                                        min_display, max_display)
+def ambient_to_display(brightness, min_ambient, max_ambient, min_display, max_display):
+    new_display_brightness = round(np.interp(brightness, [min_ambient, max_ambient], [min_display, max_display]))
+    return new_display_brightness
+
+
+def update_brightness(ambient_brightness, min_ambient, max_ambient, min_display, max_display):
+    new_brightness = ambient_to_display(ambient_brightness, min_ambient, max_ambient, min_display, max_display)
+
     set_brightness(new_brightness)
