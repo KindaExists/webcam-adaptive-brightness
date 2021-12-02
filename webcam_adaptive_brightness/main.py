3c33f36260ee170e113a503b00c7f4b866385f0a
KindaExists <ehrencastillo@gmail.com>
2021-12-02 15:53:46 +0800

    add timers and many other changes

--- webcam_adaptive_brightness/main.py
+++ webcam_adaptive_brightness/main.py
@@ -1,52 +1,90 @@
 from webcam import Webcam
+from settings import Settings
 import display
 import cv2 as cv
 import time
 
-min_ambient = 16
-max_ambient = 235
-min_display = 0
-max_display = 100
-threshold = 5
-change_period = 1   #in seconds
-
-filter_vals = []
-MAX_POINTS = 10
+settings_path = './webcam_adaptive_brightness/settings.json'
 
 
-def filter_mean(new_val):
-    global filter_vals
+class filter_mean:
+    def __init__(self, max_points=10):
+        self.vals = []
+        self.N = 0
+        self.max_points = max_points
 
-    if len(filter_vals) < MAX_POINTS:
-        filter_vals.append(new_val)
-    else:
-        filter_values = filter_vals[1:] + [new_val]
+    def insert(self, new_val):
+        if self.N < self.max_points:
+            self.vals.append(new_val)
+            self.N += 1
+        else:
+            self.vals = self.vals[1:] + [new_val]
 
-    return sum(filter_values)/len(filter_values)
+    def set_points(self, max_points=10):
+        self.max_points = max_points
+
+    def get_mean(self):
+        return sum(self.vals)/self.N
+
+
+def run():
+    # Load configuration/settings JSON file
+    configs = Settings(settings_path)
+
+    # Initialize webcam and open stream
+    wc = Webcam(configs.device)
+    wc.open()
+
+    capture = wc.get_gray()
+    wc.show_image('Window', capture)
+
+    # Initialize filter object
+    brightness_filter = filter_mean(max_points=12)
+    try:
+        # Start timers by setting them to the current time
+        loop_timer_start = time.time()
+        update_timer_start = time.time()
+        while True:
+            # Check loop timer and find current length
+            loop_timer_now = time.time()
+            loop_timer_length = loop_timer_now - loop_timer_start
+
+            # Updates values and reads camera after loop timer is done
+            if configs.loop_interval <= loop_timer_length:
+                # Get grayscaled image and display on-screen
+                cap = wc.get_gray()
+                wc.show_image('Window', cap)
+
+                # Measure brightness and insert into filter
+                measured_brightness = wc.get_brightness()
+                brightness_filter.insert(measured_brightness)
+
+                # Check update timer and find current length
+                update_timer_now = time.time()
+                update_timer_length = update_timer_now - update_timer_start
+
+                # Updates screen brightness if update timer is done
+                if configs.update_interval <= update_timer_length:
+                    # Resets update timer by setting start time to current time
+                    update_timer_start = update_timer_now
+                    ambient_brightness = brightness_filter.get_mean()
+
+                    display.update_brightness(ambient_brightness, configs.ambient, configs.display)
+
+                # Resets loop timer by setting start time to current time
+                loop_timer_start = loop_timer_now
+
+            if cv.waitKey(20) == 27:
+                break
+
+            # Sleep program for a short bit to reduce load
+            time.sleep(0.1)
+    except Exception as e:
+        print(e)
+    finally:
+        wc.release()
+        wc.close_windows()
 
 
 if __name__ == '__main__':
-    wc = Webcam(0)
-    wc.open()
-    cap = wc.get_capture()
-    wc.show_image('test', cap)
-
-    timer_start = time.time()
-    while True:
-        cap = wc.get_capture()
-        wc.show_image('test', cap)
-
-        timer_now = time.time()
-        time_interval = timer_now - timer_start
-
-        if time_interval >= change_period:
-            ambient_brightness = wc.get_brightness()
-            timer_start = timer_now
-
-            display.update_brightness(ambient_brightness, min_ambient, max_ambient, min_display, max_display)
-
-        if cv.waitKey(20) == 27:
-            break
-
-    wc.release()
-    wc.close_windows()
+    run()
