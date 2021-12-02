27567932a217f43e462c6e0489c77821ee2789d3
KindaExists <ehrencastillo@gmail.com>
2021-12-02 15:50:15 +0800

    set exposure to always be disabled

--- webcam_adaptive_brightness/webcam.py
+++ webcam_adaptive_brightness/webcam.py
@@ -8,42 +8,46 @@ class Webcam:
         self.cam = cam_ref
 
     def get_brightness(self):
-        frame = self.get_values()
+        frame = self.get_gray()
         brightness = cv.mean(frame)[0]
 
         return brightness
 
-    def get_values(self):
+    def get_gray(self):
         # Converts image from BGR to Grayscale (Y')
-        cap = self.get_capture(exposure=False)
+        cap = self.get_capture()
         gray = cv.cvtColor(cap, cv.COLOR_BGR2GRAY)
 
         return gray
 
-    def get_capture(self, exposure=True):
-        if not exposure:
+    def get_capture(self):
+        _, frame = self.vc.read()
+
+        return frame
+
+    def open(self, exposure_disabled=True):
+        self.vc.open(self.cam)
+
+        if exposure_disabled:
             # Saves exposure settings
-            def_auto = self.vc.get(cv.CAP_PROP_AUTO_EXPOSURE)
-            def_exposure = self.vc.get(cv.CAP_PROP_EXPOSURE)
+            self.def_auto = self.vc.get(cv.CAP_PROP_AUTO_EXPOSURE)
+            self.def_exposure = self.vc.get(cv.CAP_PROP_EXPOSURE)
 
             # Sets exposure to 100% and auto-exposure to 0%
             self.vc.set(cv.CAP_PROP_AUTO_EXPOSURE, 0.0)
             self.vc.set(cv.CAP_PROP_EXPOSURE, 1.0)
 
-        _, frame = self.vc.read()
-
-        if not exposure:
-            # Reverts exposure settings to original values
-            self.vc.set(cv.CAP_PROP_AUTO_EXPOSURE, def_auto)
-            self.vc.set(cv.CAP_PROP_EXPOSURE, def_exposure)
-
-        return frame
-
-    def open(self):
-        self.vc.open(self.cam)
-
-    def release(self):
+    def release(self, exposure_disabled=True):
         self.vc.release()
+
+        if exposure_disabled:
+            # Reverts exposure settings to original values
+            # self.vc.set(cv.CAP_PROP_AUTO_EXPOSURE, self.def_auto)
+            # self.vc.set(cv.CAP_PROP_EXPOSURE, self.def_exposure)
+
+            # TEMPORARY SETTERS, these are specific to my system
+            self.vc.set(cv.CAP_PROP_AUTO_EXPOSURE, 0.0)
+            self.vc.set(cv.CAP_PROP_EXPOSURE, -6.0)
 
     def show_image(self, window_name, frame):
         cv.imshow(window_name, frame)
