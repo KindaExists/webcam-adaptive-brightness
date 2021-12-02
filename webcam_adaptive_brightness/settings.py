561f70a2350427f93f4247ef3287bda0839cfa59
KindaExists <ehrencastillo@gmail.com>
2021-12-02 15:52:44 +0800

    add settings loading methods

--- webcam_adaptive_brightness/settings.py
+++ webcam_adaptive_brightness/settings.py
@@ -1,18 +1,51 @@
 import json
 
-class settings:
+class Settings:
     def __init__(self, json_path):
-        with open(json_path, 'r', encoding='utf8') as fp:
-            data = json.load(fp)
-            init_preset = data['current_preset']
-            configs = data[init_preset]
+        self.json_path = json_path
+        with open(self.json_path, 'r', encoding='utf8') as fp:
+            self.json_data = json.load(fp)
+            current_preset = self.json_data['current_preset']
+            self.open_preset(current_preset)
 
-            self.min_ambient = configs['ambient_brightness']['min']
-            self.max_ambient = configs['ambient_brightness']['max']
-            self.min_display = configs['display_brightness']['min']
-            self.max_display = configs['display_brightness']['max']
-            self.threshold = configs['threshold']
+    def open_preset(self, preset):
+        preset_config = self.json_data['presets'][preset]
 
-    def save_current(self, json_path, configs):
-        with open(json_path, 'w', encoding='utf8') as fp:
-            pass
+        self.ambient = preset_config['ambient_brightness']
+        self.display = preset_config['display_brightness']
+        self.threshold = preset_config['threshold']
+        self.loop_interval = preset_config['loop_interval']
+        self.update_interval = preset_config['update_interval']
+        self.device = preset_config['device']
+
+    def switch_preset(self, preset_name):
+        self.json_data['current_preset'] = preset_name
+        self.open_preset(preset_name)
+
+    def get_dict_repr(self):
+        return {
+            'ambient': self.ambient,
+            'display': self.display,
+            'threshold': self.threshold,
+            'loop_interval': self.loop_interval,
+            'update_interval': self.update_interval,
+            'device': self.device
+        }
+
+    def save_current(self):
+        with open(self.json_path, 'w', encoding='utf8') as fp:
+            current_preset = self.json_data['current_preset']
+            preset_config = self.json_data['presets'][current_preset]
+
+            preset_config['ambient_brightness'] = self.ambient
+            preset_config['display_brightness'] = self.display
+            preset_config['threshold'] = self.threshold
+            preset_config['loop_interval'] = self.loop_interval
+            preset_config['update_interval'] = self.update_interval
+            preset_config['device'] = self.device
+
+            json.dump(self.json_data, fp, indent=4)
+
+if __name__ == '__main__':
+    config = Settings('./webcam_adaptive_brightness/settings.json')
+    config.save_current()
