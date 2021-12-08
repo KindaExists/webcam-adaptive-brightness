03c7b0d095c92e59ea63a49971cc52bdae690f5c
KindaExists <ehrencastillo@gmail.com>
2021-12-09 00:59:59 +0800

    add display class

--- webcam_adaptive_brightness/display.py
+++ webcam_adaptive_brightness/display.py
@@ -2,19 +2,19 @@ import numpy as np
 import wmi
 
 
-def _set_screen_brightness(brightness):
-    win = wmi.WMI(namespace='wmi')
-    methods = win.WmiMonitorBrightnessMethods()[0]
-    methods.WmiSetBrightness(brightness, 0)
+class Display:
+    def set_brightness(self, brightness):
+        win = wmi.WMI(namespace='wmi')
+        methods = win.WmiMonitorBrightnessMethods()[0]
+        methods.WmiSetBrightness(brightness, 0)
 
-def _translate_ambient_to_display(brightness, minmax_ambient, minmax_display):
-    new_display_brightness = round(np.interp(brightness,
-        [minmax_ambient['min'], minmax_ambient['max']],
-        [minmax_display['min'], minmax_display['max']]))
-    return new_display_brightness
+    def _translate_from_ambient(self, brightness, minmax_ambient, minmax_display):
+        new_display_brightness = round(np.interp(brightness,
+            [minmax_ambient['min'], minmax_ambient['max']],
+            [minmax_display['min'], minmax_display['max']]))
+        return new_display_brightness
 
-def update_brightness(ambient_brightness, minmax_ambient, minmax_display):
-    new_display_brightness = _translate_ambient_to_display(ambient_brightness, minmax_ambient, minmax_display)
-    _set_screen_brightness(new_display_brightness)
-
+    def update_from_ambient(self, ambient_brightness, minmax_ambient, minmax_display):
+        new_display_brightness = self._translate_from_ambient(ambient_brightness, minmax_ambient, minmax_display)
+        self.set_brightness(new_display_brightness)
 
