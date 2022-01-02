9d3030f614379d959f21ff765ab77c016765e2e8
KindaExists <ehrencastillo@gmail.com>
2022-01-02 18:15:38 +0800

    fix alignment & other GUi issues

--- webcam_adaptive_brightness/user_Interface/main.py
+++ webcam_adaptive_brightness/user_Interface/main.py
@@ -26,10 +26,20 @@ def round_rectangle(canvas,x1, y1, x2, y2, radius=25, **kwargs):
     return canvas.create_polygon(points, **kwargs, smooth=True)
 def opensettings():
    messagebox.showinfo("Settings", "just imagine ur in d settings")
-
+def changeOnHover(button, colorOnHover, colorOnLeave):
+  
+    # adjusting backgroung of the widget
+    # background on entering widget
+    button.bind("<Enter>", func=lambda e: button.config(background=colorOnHover))
+    # background color on leving widget
+    button.bind("<Leave>", func=lambda e: button.config(background=colorOnLeave))
 
 webcamvalue=100 #webcam brightness value
 screenvalue=100 #screen brightness value
+lightGrey="#3A3A3A"
+grey="#262626"
+fgBlue="#5B9BD5"
+
 
 root = Tk()
 root.resizable(False,False)
@@ -37,33 +47,73 @@ root.title("WAABP")
 positionRight = int(root.winfo_screenwidth()/2 - 880/2)
 positionDown = int(root.winfo_screenheight()/2 - 495/2)
 root.geometry("880x495+{}+{}".format(positionRight, positionDown))
-root.configure(background='#262626',highlightcolor='#5B9BD5',highlightthickness=1)
+root.configure(background='#262626',highlightcolor=fgBlue,highlightthickness=1)
 
-header= Frame(root, highlightbackground='#5B9BD5', bg='#262626', highlightcolor='#5B9BD5', highlightthickness=1, width=880, height=55, bd= 0)
+#bgcolor=#262626 dark grey, #3A3A3A light grey
+
+
+
+######## header ########
+header= Frame(root, highlightbackground=fgBlue, bg='#262626', highlightcolor=fgBlue, highlightthickness=1, width=880, height=55, bd= 0)
+header.pack_propagate(0)
 header.pack()
 
-title = Label(header, text = "WAABP:", font='Bahnschrift 14 bold', bg='#262626',fg='#FFFFFF').place(x = 20, y = 11)
-subtitle = Label(header, text = "A Webcam-based Adjustable Adaptive Brightness Program", font='Bahnschrift 10', bg='#262626',fg='#FFFFFF').place(x = 95, y = 17)  
-settings = Button(header, font='Bahnschrift 10', bg='#262626',fg='#FFFFFF',text = "Settings", padx=5,pady=3, command = opensettings).place(x=790,y=11)
+title = Frame(header,width=80,height=30,bg=grey)
+title.pack_propagate(0)
+Label(title,bg=grey,fg="white",text="WAABP:",font=("Bahnschrift Bold",14)).pack()
+title.pack(side=LEFT,padx=(15,0))
 
-middle= Canvas(root, highlightbackground='#5B9BD5', bg='#262626', highlightcolor='#5B9BD5', highlightthickness=1, width=880, height=385, bd= 0)
+subtitle = Frame(header,width=340,height=20,bg=grey)
+subtitle.pack_propagate(0)
+Label(subtitle, text = "A Webcam-based Adjustable Adaptive Brightness Program", font=("Bahnschrift Light",10), bg=grey,fg='white').pack()
+subtitle.pack(side=LEFT,padx=0,pady=(6,0))
+
+settings = Frame(header,width=70,height=30,bg=grey,highlightbackground=fgBlue,highlightthickness=1)
+settings.pack_propagate(0)
+settingsButton=Button(settings, font=("Bahnschrift",10), bg='#262626',fg='#FFFFFF',text = "Settings", padx=5,pady=3, command = opensettings,activebackground=fgBlue,activeforeground="black",borderwidth = 0)
+settingsButton.pack(fill=X)
+settings.pack(side=RIGHT,padx=(0,15))
+changeOnHover(settingsButton, "#364A5B", grey)
+
+######## middle ########
+middle= Canvas(root, highlightbackground=fgBlue, bg='#262626', highlightcolor=fgBlue, highlightthickness=1, width=880, height=385, bd= 0)
 middle.pack(side=TOP)
 
-webcambox1=round_rectangle(middle,35,40,230,102,radius=20,fill='#3A3A3A',outline='#595959')
-webcamlabel = Label(middle, text = "Current Webcam\n Brightness", font='{Bahnschrift Light} 12', bg='#3A3A3A',fg='#FFFFFF').place(x = 70, y = 47)
-webcambox2=round_rectangle(middle,35,120,230,340,radius=20,fill='#3A3A3A',outline='#595959')
-webcamvaluelabel = Label(middle, text = "{}%".format(webcamvalue), font='{Bahnschrift Light} 50', bg='#3A3A3A',fg='#FFFFFF',width=4,justify='center').place(x = 57, y = 180)
+webcamF1=Frame(middle,width=187,height=44,bg=lightGrey)
+webcamF1.pack_propagate(0)
+Label(webcamF1, text = "Current Webcam\nBrightness", font='{Bahnschrift Light} 12', bg=lightGrey,fg='#FFFFFF').pack()
+webcamF1.place(x=40,y=49)
+webcamB1=round_rectangle(middle,35,40,230,102,radius=20,fill=lightGrey,outline='#595959')
 
-screenbox1=round_rectangle(middle,248,40,443,102,radius=20,fill='#3A3A3A',outline='#595959')
-screenlabel = Label(middle, text = "Calculated Screen\n Brightness", font='{Bahnschrift Light} 12', bg='#3A3A3A',fg='#FFFFFF').place(x = 275, y = 47)
-screenbox2=round_rectangle(middle,248,120,443,340,radius=20,fill='#3A3A3A',outline='#595959')
-screenvaluelabel = Label(middle, text = "{}%".format(screenvalue), font='{Bahnschrift Light} 50', bg='#3A3A3A',fg='#FFFFFF',width=4,justify='center').place(x = 269, y = 180)
+webcamF2 = Frame(middle,width=187,height=87,bg=lightGrey)
+webcamF2.pack_propagate(0)
+Label(webcamF2, text="{}%".format(webcamvalue), font='{Bahnschrift Light} 50', bg=lightGrey,fg='#FFFFFF',width=4,justify='center').pack()
+webcamF2.place(x=40,y=185)
+webcamB2=round_rectangle(middle,35,120,230,340,radius=20,fill=lightGrey,outline='#595959')
 
-previewbox=round_rectangle(middle,461,40,845,340,radius=20,fill='#3A3A3A',outline='#595959')
-previewlabel = Label(middle, text = "Webcam Preview", font='{Bahnschrift Light} 12', bg='#3A3A3A',fg='#FFFFFF').place(x = 590, y = 50)
-webcamFrame = Frame(middle, width=300, height=225,bg='#000').place(x=505,y=85)
+screenF1 = Frame(middle,width=188,height=44,bg=lightGrey)
+screenF1.pack_propagate(0)
+Label(screenF1, text = "Calculated Screen\nBrightness", font='{Bahnschrift Light} 12', bg=lightGrey,fg='#FFFFFF').pack()
+screenF1.place(x=252,y=49)
+screenB1=round_rectangle(middle,248,40,443,102,radius=20,fill=lightGrey,outline='#595959')
 
-bottom= Frame(root, highlightbackground='#5B9BD5', bg='#262626', highlightcolor='#5B9BD5', highlightthickness=1, width=880, height=55, bd= 0)
+screenF2 = Frame(middle,width=187,height=87,bg=lightGrey)
+screenF2.pack_propagate(0)
+Label(screenF2, text = "{}%".format(screenvalue), font='{Bahnschrift Light} 50', bg=lightGrey,fg='#FFFFFF',width=4,justify='center').pack()
+screenF2.place(x=252,y=185)
+screenbox2=round_rectangle(middle,248,120,443,340,radius=20,fill=lightGrey,outline='#595959')
+
+previewbox=round_rectangle(middle,461,40,845,340,radius=20,fill=lightGrey,outline='#595959')
+preview = Frame(middle,width=376,height=25,bg=lightGrey)
+preview.pack_propagate(0)
+Label(preview, text = "Webcam Preview", font='{Bahnschrift Light} 12', bg=lightGrey,fg='#FFFFFF').pack(fill=BOTH)
+preview.place(x=465,y=50)
+
+webcamFrame = Frame(middle, width=300, height=225,bg='#000')
+webcamFrame.place(x=505,y=85)
+
+######## bottom ########
+bottom= Frame(root, highlightbackground=fgBlue, bg='#262626', highlightcolor=fgBlue, highlightthickness=1, width=880, height=55, bd= 0)
 bottom.pack(side=TOP)
 
 root.mainloop()
