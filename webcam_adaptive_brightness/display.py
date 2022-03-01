411f6625fbe2872abdf9e96fdffb45f9478bde66
KindaExists <ehrencastillo@gmail.com>
2022-03-01 14:20:45 +0800

    remove commented code

--- webcam_adaptive_brightness/display.py
+++ webcam_adaptive_brightness/display.py
@@ -10,12 +10,6 @@ class Display:
         self.screen_range = self.__get_screen_brightness_range()
         self.ambient_range = (0.0, 255.0)
 
-        """
-        self.prev_brightness = -1
-        self.was_overriden = False
-        self.deviation_direction = 0
-        """
-
     def __get_screen_brightness_range(self):
         win = wmi.WMI(namespace='wmi')
         monitor = win.WmiMonitorBrightness()[self.screen_id]
@@ -55,45 +49,3 @@ class Display:
             minmax_screen,
         )
         self.__set_screen_brightness(new_screen_brightness)
-
-        """
-        cur_screen_brightness = self.get_current_brightness()
-        new_screen_brightness = self.__translate_from_ambient(
-            ambient_brightness, minmax_ambient)
-
-        if self.prev_brightness < 0:
-            self.prev_brightness = new_screen_brightness
-            self.set_brightness(new_screen_brightness)
-            return
-
-        # Checks if screen brightness matches with the
-        # screen brightness from the previous update
-        # - If yes, the user changed the brightness manually
-        # - If no, the user did not change brightness between updates
-        if (self.prev_brightness != cur_screen_brightness and
-                not self.was_overriden):
-            self.was_overriden = True
-
-            diff = cur_screen_brightness - self.prev_brightness
-            self.deviation_direction = 1 if diff > 0 else -1
-
-        if self.was_overriden:
-            cur_diff = new_screen_brightness - cur_screen_brightness
-            cur_deviation_direction = 1 if cur_diff > 0 else -1
-
-            if self.deviation_direction > 0 and cur_deviation_direction > 0:
-                if new_screen_brightness >= cur_screen_brightness:
-                    self.was_overriden = False
-                    self.deviation_direction = 0
-
-            elif self.deviation_direction < 0 and cur_deviation_direction < 0:
-                if new_screen_brightness <= cur_screen_brightness:
-                    self.was_overriden = False
-                    self.deviation_direction = 0
-
-        # Does not update prev_brightness or screen brightness
-        # if manual override was done
-        if not self.was_overriden:
-            self.prev_brightness = new_screen_brightness
-            self.set_brightness(new_screen_brightness)
-        """
